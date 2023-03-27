390a9e3ae5c44b0ad7180186f158dbced151ebcf
Yu19980809 <1572250276@qq.com>
2023-03-27 19:03:07 +0800

    Build project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect } from "react";
+import "../src/assets/icons/font-awesome.css";
+import "./App.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Home from "./pages/Home";
+import AOS from "aos";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 2000 })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Home />
   );
 }
 
