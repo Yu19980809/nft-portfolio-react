f3b352641c6cd4cec871dfebb219c9b602911abd
Yu19980809 <1572250276@qq.com>
2023-03-30 15:59:02 +0800

    Finished navbar section

---
+++ src/assets/data/menu.js
@@ -0,0 +1,29 @@
+const menus = [
+    {
+        id: 1,
+        name: 'Home',
+        links: '#'
+    },
+    {
+        id: 2,
+        name: 'About',
+        links: 'About'
+    },
+    {
+        id: 3,
+        name: 'Collections',
+        links: 'Collections'
+    },
+    {
+        id: 4,
+        name: 'Pages',
+        links: '#'
+    },
+    {
+        id: 5,
+        name: 'Contact',
+        links: 'Contact'
+    }
+];
+
+export default menus;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 import './header.css';
+import logo from '../../assets/data/logo';
+import menus from '../../assets/data/menu';
+import Button from '../button/ButtonOne';
 
 const Header = () => {
+    const [ scroll, setScroll ]  = useState(false);
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setScroll(window.scrollY > 100);
+        });
+
+        return () => { setScroll({}); };
+    }, []);
+
+    const [menuActive, setMenuActive] = useState(null);
+    const handleMenuActive = () => {
+        setMenuActive(!menuActive);
+    };
+
     return (
-        <div>Header</div>
+        <header id='header_main' className={`header js-header ${ scroll ? 'is-fixed' : '' }`}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="header__body d-flex justify-content-between">
+                            <div className="header__logo">
+                                <Link to='/'>
+                                    <img src={ logo } id="site-logo" alt="" />
+                                </Link>
+                            </div>
+                            <div className="header__right">
+                                <nav id="main-nav" className={ `main-nav ${menuActive ? 'active' : ''}` }>
+                                    <ul id="menu-primary-menu" className="menu">
+                                        {
+                                            menus.map((item, index) => {
+                                                return <li key={ index } className={ `menu-item` }>
+                                                    <a href={ item.links }>{ item.name }</a>
+                                                </li>
+                                            })
+                                        }
+                                    </ul>
+                                </nav>
+                                <div className="button">
+                                    <Button />
+                                </div>
+                                <div
+                                    className={ `mobile-button ${ menuActive ? 'active' : '' }` }
+                                    onClick={ handleMenuActive }
+                                >
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
