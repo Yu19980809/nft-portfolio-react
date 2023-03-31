729b4e153d903ea75ce5ef8db6634e07d633bbfb
Yu19980809 <1572250276@qq.com>
2023-03-31 14:24:50 +0800

    Finished abour section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/header/header";
-import Slider from "../components/slider/slider"
+import Slider from "../components/slider/slider";
+import About from "../layouts/About";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Slider />
+            <About />
         </div>
     );
 }
